f20b93ffc6f5971381f1606fae79e5696bb47f66
tuepon <t.uepon@gmail.com>
2023-12-11 21:45:00 +0900

    update destroy method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -35,6 +35,9 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
