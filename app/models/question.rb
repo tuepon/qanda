bb34f54f7e539ddd41f39b5543a4e4ffe424948f
tuepon <t.uepon@gmail.com>
2023-12-12 21:22:18 +0900

    update question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+
   validates :title, presence: true
   validates :name, presence: true
   validates :content, presence: true, length: { minimum: 5 }
+
 end
